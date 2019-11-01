370b422a2fef1a73f459884c50fbf917bc49755e
Cesar Soares Lucas <disoares@microsoft.com>
2019-11-01 10:56:46 -0700

    Addressing PR feedback.

--- src/core-sdk-tasks/DownloadFile.cs
+++ src/core-sdk-tasks/DownloadFile.cs
@@ -57,35 +57,49 @@ namespace Microsoft.DotNet.Cli.Build
                 return true;
             }
 
-            Log.LogMessage(MessageImportance.High, $"Downloading '{Uri}' to '{DestinationPath}'");
+            var lastStatus = await DownloadWithRetries(Uri, DestinationPath);
+            if (lastStatus == false)
+            {
+                if (!string.IsNullOrEmpty(PrivateUri))
+                {
+                    lastStatus = await DownloadWithRetries(PrivateUri, DestinationPath);
+                }
+            }
 
+            return lastStatus == true;
+        }
+
+        /// <summary>
+        /// Attempt to download file from `source` with retries when response error is different of FileNotFound and Success.
+        /// </summary>
+        /// <param name="source">URL to the file to be downloaded.</param>
+        /// <param name="target">Local path where to put the downloaded file.</param>
+        /// <returns>true: Download Succeeded. false: Download failed with 404. null: Download failed but is retriable.</returns>
+        private async Task<bool?> DownloadWithRetries(string source, string target)
+        {
             Random rng = new Random();
+
+            Log.LogMessage(MessageImportance.High, $"Attempting download '{source}' to '{target}'");
+
             for (int retryNumber = 0; retryNumber < MaxRetries; retryNumber++)
             {
                 using (var httpClient = new HttpClient())
                 {
                     try
                     {
-                        var httpResponse = await httpClient.GetAsync(Uri);
+                        var httpResponse = await httpClient.GetAsync(source);
 
-                        if (httpResponse.StatusCode == HttpStatusCode.NotFound)
+                        // The Azure Storage REST API returns '400 - Bad Request' in some cases
+                        // where the resource is not found on the storage.
+                        // https://docs.microsoft.com/en-us/rest/api/storageservices/common-rest-api-error-codes
+                        if (httpResponse.StatusCode == HttpStatusCode.NotFound ||
+                            httpResponse.ReasonPhrase.IndexOf("The requested URI does not represent any resource on the server.", StringComparison.OrdinalIgnoreCase) >= 0)
                         {
-                            if (!string.IsNullOrEmpty(PrivateUri))
-                            {
-                                httpResponse = await httpClient.GetAsync(PrivateUri);
-                            }
-
-                            // The Azure Storage REST API returns '400 - Bad Request' in some cases
-                            // where the resource is not found on the storage.
-                            // https://docs.microsoft.com/en-us/rest/api/storageservices/common-rest-api-error-codes
-                            if (httpResponse.StatusCode == HttpStatusCode.NotFound ||
-                                httpResponse.ReasonPhrase.IndexOf("The requested URI does not represent any resource on the server.", StringComparison.OrdinalIgnoreCase) >= 0)
-                            {
-                                return false;
-                            }
+                            Log.LogMessage($"File not found error while attempting download using the following URL(s): '{source}'.");
+                            return false;
                         }
 
-                        using (var outStream = File.Create(DestinationPath))
+                        using (var outStream = File.Create(target))
                         {
                             await httpResponse.Content.CopyToAsync(outStream);
                         }
@@ -94,16 +108,16 @@ namespace Microsoft.DotNet.Cli.Build
                     }
                     catch (Exception e)
                     {
-                        var secondaryUri = !string.IsNullOrWhiteSpace(PrivateUri) ? $"/'{PrivateUri}'" : string.Empty;
-                        Log.LogMessage($"Problems downloading file from '{Uri}'{secondaryUri}. {e.Message} {e.StackTrace}");
-                        File.Delete(DestinationPath);
+                        Log.LogMessage($"Problems downloading file from '{source}'. {e.Message} {e.StackTrace}");
+                        File.Delete(target);
                     }
                 }
 
-                await System.Threading.Tasks.Task.Delay(rng.Next(3000, 10000));
+                await System.Threading.Tasks.Task.Delay(rng.Next(1000, 10000));
             }
 
-            return false;
+            Log.LogMessage($"Giving up downloading the file from '{source}' after {MaxRetries} retries.");
+            return null;
         }
     }
 }
