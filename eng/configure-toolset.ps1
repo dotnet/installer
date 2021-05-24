7c79ce28bc5f42214c49e2ebbf3d7768ca927fa2
Jacques Eloff <joeloff@users.noreply.github.com>
2021-05-24 14:05:11 -0700

    Create WASM installers for optional workloads (#10685)
    Co-authored-by: Rainer Sigwald <raines@microsoft.com>

--- eng/configure-toolset.ps1
+++ eng/configure-toolset.ps1
@@ -4,3 +4,9 @@ $script:useInstalledDotNetCli = $false
 
 # Add CMake to the path.
 $env:PATH = "$PSScriptRoot\..\.tools\bin;$env:PATH"
+
+if ($msbuildEngine -eq 'vs')
+{
+    $globalJson = Get-Content -Raw global.json | ConvertFrom-Json
+    $env:MSBuildSdksPath = [IO.Path]::Combine($RepoRoot, ".dotnet\sdk", $globalJson.tools.dotnet, "sdks")
+}
