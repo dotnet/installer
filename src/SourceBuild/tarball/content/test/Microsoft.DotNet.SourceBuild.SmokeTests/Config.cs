83e0bc99a48649bbd510b8b4423aee80c5be58b9
MichaelSimons <msimons@microsoft.com>
2022-02-04 00:52:27 +0000

    Edit per code review feedback

--- src/SourceBuild/tarball/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/Config.cs
+++ src/SourceBuild/tarball/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/Config.cs
@@ -11,6 +11,7 @@ internal static class Config
     public static string DotNetDirectory { get; } = Environment.GetEnvironmentVariable("DOTNET_DIR") ?? "./.dotnet";
     public static string DotNetTarballPath { get; } = Environment.GetEnvironmentVariable(DotNetTarballPathEnv) ?? string.Empty;
     public const string DotNetTarballPathEnv = "DOTNET_TARBALL_PATH";
-    public static bool ExcludeOnlineTests { get; } = Environment.GetEnvironmentVariable("EXCLUDE_ONLINE_TESTS") != null;
+    public static bool ExcludeOnlineTests { get; } =
+        bool.TryParse(Environment.GetEnvironmentVariable("EXCLUDE_ONLINE_TESTS"), out bool excludeOnlineTests) ? excludeOnlineTests : false;
     public static string TargetRid { get; } = Environment.GetEnvironmentVariable("TARGET_RID") ?? string.Empty;
 }
