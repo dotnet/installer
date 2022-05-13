13af995207072c67c709d82f4576d87ec657cbc2
Michael Simons <msimons@microsoft.com>
2022-05-13 08:45:32 -0500

    Refactor to add cumulative source-build package sources

--- src/SourceBuild/tarball/content/tools-local/tasks/Microsoft.DotNet.SourceBuild.Tasks.XPlat/RemoveInternetSourcesFromNuGetConfig.cs
+++ src/SourceBuild/tarball/content/tools-local/tasks/Microsoft.DotNet.SourceBuild.Tasks.XPlat/RemoveInternetSourcesFromNuGetConfig.cs
@@ -42,7 +42,6 @@ namespace Microsoft.DotNet.Build.Tasks
             XDocument d = XDocument.Parse(xml);
             XElement packageSourcesElement = d.Root.Descendants().First(e => e.Name == "packageSources");
             XElement disabledPackageSourcesElement = d.Root.Descendants().FirstOrDefault(e => e.Name == "disabledPackageSources");
-            XElement packageSourceMappingElement = d.Root.Descendants().FirstOrDefault(e => e.Name == "packageSourceMapping");
 
             IEnumerable<XElement> local = packageSourcesElement.Descendants().Where(e =>
             {
@@ -75,9 +74,6 @@ namespace Microsoft.DotNet.Build.Tasks
 
             // Remove disabledPackageSources element so if any internal packages remain, they are used in source-build
             disabledPackageSourcesElement?.ReplaceNodes(new XElement("clear"));
-
-            // Do the same for packageSourceMappings, these are re-added in AddSourceToNuGetConfig
-            packageSourceMappingElement?.ReplaceNodes(new XElement("clear"));
 
             using (var w = XmlWriter.Create(NuGetConfigFile, new XmlWriterSettings { NewLineChars = newLineChars, Indent = true }))
             {
