3e040ce37f302f843a7fbc6ab821360f219fe1dd
Premek Vysoky <premek.vysoky@microsoft.com>
2022-08-12 12:03:53 +0200

    Add empty InitializeVMR targets, VMR tasks

---
+++ src/VirtualMonoRepo/VirtualMonoRepo_Initialize.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.DotNet.DarcLib.VirtualMonoRepo;
+
+namespace Microsoft.DotNet.VirtualMonoRepo.Tasks;
+
+public class VirtualMonoRepo_Initialize : Task
+{
+    [Required]
+    public string Repository { get; set; }
+
+    public string Revision { get; set; }
+
+    public override bool Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
