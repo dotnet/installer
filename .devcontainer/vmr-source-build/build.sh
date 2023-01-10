934c04d0622ade8924ff90c21b42cebaf2e38daf
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-10 17:25:53 +0100

    Add openssl1.1.x86_64

--- .devcontainer/vmr-source-build/build.sh
+++ .devcontainer/vmr-source-build/build.sh
@@ -3,14 +3,14 @@
 source="${BASH_SOURCE[0]}"
 script_root="$( cd -P "$( dirname "$source" )" && pwd )"
 
+# TODO: Move this into the image?
+dnf -y install openssl1.1.x86_64
+
 repo_root=$(realpath "$script_root/../..")
-tmp_dir="$repo_root/artifacts/tmp"
-vmr_dir="$tmp_dir/artifacts/tmp/dotnet"
+tmp_dir=$(realpath "$repo_root/../tmp")
+vmr_dir=$(realpath "$repo_root/../dotnet")
 
-# git -C "$repo_root" fetch --all
-
-# "$script_root"/../../eng/vmr-sync.sh \
-#     --vmr "$vmr_dir" \
-#     --tmp "$tmp_dir" \
-#     --branch main    \
-#     --debug
+"$script_root"/../../eng/vmr-sync.sh \
+    --vmr "$vmr_dir" \
+    --tmp "$tmp_dir" \
+    --branch main
