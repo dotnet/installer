5a47d349f91b3ec39082aa2d9b290ae21a7fa985
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-10 16:54:35 +0100

    Do not sync the VMR

--- .devcontainer/vmr-source-build/build.sh
+++ .devcontainer/vmr-source-build/build.sh
@@ -7,10 +7,10 @@ repo_root=$(realpath "$script_root/../..")
 tmp_dir="$repo_root/artifacts/tmp"
 vmr_dir="$tmp_dir/artifacts/tmp/dotnet"
 
-git -C "$repo_root" fetch --all
+# git -C "$repo_root" fetch --all
 
-"$script_root"/../../eng/vmr-sync.sh \
-    --vmr "$vmr_dir" \
-    --tmp "$tmp_dir" \
-    --branch main    \
-    --debug
+# "$script_root"/../../eng/vmr-sync.sh \
+#     --vmr "$vmr_dir" \
+#     --tmp "$tmp_dir" \
+#     --branch main    \
+#     --debug
