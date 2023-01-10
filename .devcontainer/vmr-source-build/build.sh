a05e9cfa6c2ca67d012991e7c0ff08f204e6e890
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-10 18:23:19 +0100

    Crazy dance

--- .devcontainer/vmr-source-build/build.sh
+++ .devcontainer/vmr-source-build/build.sh
@@ -6,11 +6,20 @@ script_root="$( cd -P "$( dirname "$source" )" && pwd )"
 # TODO: Move this into the image?
 dnf -y install openssl1.1.x86_64
 
-installer_root=$(realpath "$script_root/../..")
-tmp_dir=$(realpath "$installer_root/../tmp")
-vmr_dir=$(realpath "$installer_root/../dotnet")
+installer_dir=$(realpath "$script_root/../..")
+tmp_dir=$(realpath "$installer_dir/../tmp")
+vmr_dir=$(realpath "$installer_dir/../dotnet")
 
-git -C "$installer_root" fetch --all
+mkdir -p "$tmp_dir"
+
+installer_sha=$(git -C "$installer_dir" rev-parse HEAD)
+cp "$installer_dir/.git/config" "$tmp_dir/git_config"
+rm -rf "$installer_dir"
+mkdir -p "$installer_dir"
+git -C "$installer_dir" init
+mv "$tmp_dir/git_config" "$installer_dir/.git/config"
+git -C "$installer_dir" fetch --all
+git -C "$installer_dir" checkout "$installer_sha"
 
 "$script_root"/../../eng/vmr-sync.sh \
     --vmr "$vmr_dir" \
