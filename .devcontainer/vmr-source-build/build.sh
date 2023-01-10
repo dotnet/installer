e4cbcf6ec67a68a80852edc021349ab2d34021db
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-10 15:45:15 +0100

    Try with main first

--- .devcontainer/vmr-source-build/build.sh
+++ .devcontainer/vmr-source-build/build.sh
@@ -7,11 +7,10 @@ repo_root=$(realpath "$script_root/../..")
 tmp_dir="$repo_root/artifacts/tmp"
 vmr_dir="$tmp_dir/artifacts/tmp/dotnet"
 
-# We will try to figure out, which branch is the current (PR) branch based off of
-# We need this to figure out, which VMR branch to use
-vmr_branch=$(git log --pretty=format:'%D' HEAD^ | grep 'origin/' | head -n1 | sed 's@origin/@@' | sed 's@,.*@@')
+git -C "$repo_root" fetch --all
 
 "$script_root"/../../eng/vmr-sync.sh \
-    --vmr "$vmr_dir"                \
-    --tmp "$tmp_dir"                \
-    --branch "$vmr_branch"
+    --vmr "$vmr_dir" \
+    --tmp "$tmp_dir" \
+    --branch main    \
+    --debug
