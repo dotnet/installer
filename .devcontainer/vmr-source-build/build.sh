eb407efc64287b2ca2c0cc9837fa38f03c042d7d
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-10 17:34:20 +0100

    Fetch all anyway

--- .devcontainer/vmr-source-build/build.sh
+++ .devcontainer/vmr-source-build/build.sh
@@ -6,9 +6,11 @@ script_root="$( cd -P "$( dirname "$source" )" && pwd )"
 # TODO: Move this into the image?
 dnf -y install openssl1.1.x86_64
 
-repo_root=$(realpath "$script_root/../..")
-tmp_dir=$(realpath "$repo_root/../tmp")
-vmr_dir=$(realpath "$repo_root/../dotnet")
+installer_root=$(realpath "$script_root/../..")
+tmp_dir=$(realpath "$installer_root/../tmp")
+vmr_dir=$(realpath "$installer_root/../dotnet")
+
+git -C "$installer_root" fetch --all
 
 "$script_root"/../../eng/vmr-sync.sh \
     --vmr "$vmr_dir" \
