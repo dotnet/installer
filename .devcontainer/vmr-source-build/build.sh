a51c25755d43c9ea3eea4274410e0baaee4e10db
Premek Vysoky <premek.vysoky@microsoft.com>
2023-01-16 10:53:19 +0100

    Try if fetching works now

--- .devcontainer/vmr-source-build/build.sh
+++ .devcontainer/vmr-source-build/build.sh
@@ -5,24 +5,25 @@ set -ex
 source="${BASH_SOURCE[0]}"
 script_root="$( cd -P "$( dirname "$source" )" && pwd )"
 
-# TODO: Move this into the image?
+# TODO: Remove once we have a new version of the image
 dnf -y install openssl1.1.x86_64
 
 installer_dir=$(realpath "$script_root/../..")
-tmp_dir=$(realpath "$installer_dir/../tmp")
-vmr_dir=$(realpath "$installer_dir/../dotnet")
+workspace_dir=$(realpath "$installer_dir/../")
+tmp_dir=$(realpath "$workspace_dir/tmp")
+vmr_dir=$(realpath "$workspace_dir/dotnet")
 
 mkdir -p "$tmp_dir"
 
-installer_sha=$(git -C "$installer_dir" rev-parse HEAD)
-cp "$installer_dir/.git/config" "$tmp_dir/git_config"
-rm -rf "$installer_dir"
-mkdir -p "$installer_dir"
-pushd "$installer_dir"
-git init
-mv "$tmp_dir/git_config" ./.git/config
-git fetch --all
-git checkout "$installer_sha"
+# installer_sha=$(git -C "$installer_dir" rev-parse HEAD)
+# cp "$installer_dir/.git/config" "$tmp_dir/git_config"
+# rm -rf "$installer_dir"
+# mkdir -p "$installer_dir"
+# pushd "$installer_dir"
+# git init
+# mv "$tmp_dir/git_config" ./.git/config
+# git fetch --all
+# git checkout "$installer_sha"
 
 ./eng/vmr-sync.sh    \
     --vmr "$vmr_dir" \
