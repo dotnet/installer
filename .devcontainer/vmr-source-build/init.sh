07376f96bdfba91953eaaad9408a4c812d6cdc84
Premek Vysoky <premek.vysoky@microsoft.com>
2023-02-16 14:25:42 +0100

    Fix TPN/README

--- .devcontainer/vmr-source-build/init.sh
+++ .devcontainer/vmr-source-build/init.sh
@@ -17,7 +17,11 @@ git -C "$installer_dir" fetch --all --unshallow
 
 # We will try to figure out, which branch is the current (PR) branch based off of
 # We need this to figure out, which VMR branch to use
-vmr_branch=$(git log --pretty=format:'%D' HEAD^ | grep 'origin/' | head -n1 | sed 's@origin/@@' | sed 's@,.*@@')
+vmr_branch=$(git -C "$installer_dir" log --pretty=format:'%D' HEAD^ \
+  | grep 'origin/' \
+  | head -n1 \
+  | sed 's@origin/@@' \
+  | sed 's@,.*@@')
 
 pushd "$installer_dir"
   "./eng/vmr-sync.sh"      \
