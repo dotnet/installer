57976db13d4246e2d19a4880b01ea2a603405e58
Michael Yanni <miyanni@microsoft.com>
2024-02-28 15:32:27 -0800

    Removing the prior native tools workarounds as per https://github.com/dotnet/installer/pull/18525

--- eng/configure-toolset.ps1
+++ eng/configure-toolset.ps1
@@ -1,6 +1,3 @@
 # SdkTests do not currently work with globally installed CLI as they use dotnet-install.ps1 to install more runtimes
 
 $script:useInstalledDotNetCli = $false
-
-# Add CMake to the path.
-$env:PATH = "$PSScriptRoot\..\.tools\bin;$env:PATH"
--- eng/configure-toolset.sh
+++ eng/configure-toolset.sh
@@ -1,6 +1,3 @@
 # SdkTests do not currently work with globally installed CLI as they use dotnet-install.ps1 to install more runtimes
 
 useInstalledDotNetCli="false"
-
-# Working around issue https://github.com/dotnet/arcade/issues/7327
-DisableNativeToolsetInstalls=true
