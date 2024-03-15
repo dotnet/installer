aca71de46211eb2ad5fbb71744e74a0eedb3742c
dkurepa <dkurepa@microsoft.com>
2024-03-15 11:05:23 +0100

    Create and publish VerticalAssetManifest

---
+++ src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest/PublishVerticalManifest.cs
@@ -0,0 +1,45 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest
+{
+    public class PublishVerticalManifest : Task
+    {
+        public string AssetManifestsLocalStorageDir { get; set; }
+        public string VerticalAssetManifestLocalPath { get; set; }
+
+        public override bool Execute()
+        {
+            var assetManifestXmls = Directory.GetFiles(AssetManifestsLocalStorageDir, "*.xml", SearchOption.AllDirectories)
+                .Select(xmlPath => XDocument.Load(xmlPath));
+
+            var rootName = assetManifestXmls.First().Root.Name;
+            var rootAttributes = assetManifestXmls.First().Root.Attributes();
+            var packageElements = new List<XElement>();
+            var blobElements = new List<XElement>();
+
+            foreach (var assetManifestXml in assetManifestXmls)
+            {
+                packageElements.AddRange(assetManifestXml.Descendants("Package"));
+                blobElements.AddRange(assetManifestXml.Descendants("Blob"));
+            }
+            
+            packageElements.OrderBy(packageElement => packageElement.Attribute("Id").Value);
+            blobElements.OrderBy(blobElement => blobElement.Attribute("Id").Value);
+
+            var verticalManifest = new XDocument(new XElement(rootName, rootAttributes, packageElements, blobElements));
+
+            File.WriteAllText(VerticalAssetManifestLocalPath, verticalManifest.ToString());
+
+            return true;
+        }
+    }
+}
