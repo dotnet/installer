18fe9984addcdfcbc1e272ab3382647ab29a8a17
dkurepa <dkurepa@microsoft.com>
2024-03-18 13:35:58 +0100

    Resolve some comments

--- src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest/PublishVerticalManifest.cs
+++ src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest/PublishVerticalManifest.cs
@@ -5,6 +5,7 @@
 using Microsoft.Build.Utilities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
@@ -13,13 +14,15 @@ namespace Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest
 {
     public class PublishVerticalManifest : Task
     {
+        [Required]
         public string AssetManifestsLocalStorageDir { get; set; }
+        [Required]
         public string VerticalAssetManifestLocalPath { get; set; }
 
         public override bool Execute()
         {
             var assetManifestXmls = Directory.GetFiles(AssetManifestsLocalStorageDir, "*.xml", SearchOption.AllDirectories)
-                .Select(xmlPath => XDocument.Load(xmlPath));
+                .Select(xmlPath => XDocument.Load(xmlPath)).ToList();
 
             var rootName = assetManifestXmls.First().Root.Name;
             var rootAttributes = assetManifestXmls.First().Root.Attributes();
@@ -32,8 +35,8 @@ namespace Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest
                 blobElements.AddRange(assetManifestXml.Descendants("Blob"));
             }
             
-            packageElements.OrderBy(packageElement => packageElement.Attribute("Id").Value);
-            blobElements.OrderBy(blobElement => blobElement.Attribute("Id").Value);
+            packageElements = packageElements.OrderBy(packageElement => packageElement.Attribute("Id").Value).ToList();
+            blobElements = blobElements.OrderBy(blobElement => blobElement.Attribute("Id").Value).ToList();
 
             var verticalManifest = new XDocument(new XElement(rootName, rootAttributes, packageElements, blobElements));
 
