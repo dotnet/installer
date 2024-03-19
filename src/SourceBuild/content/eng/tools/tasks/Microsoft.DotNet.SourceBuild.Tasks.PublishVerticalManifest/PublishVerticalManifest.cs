439a5f17347153ee6439ff5a8b3e60d871fc13ec
dkurepa <dkurepa@microsoft.com>
2024-03-19 14:01:18 +0100

    Address comments, ignore AzureDevOpsBuildNumber and BuildId, check attribute count

--- src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest/PublishVerticalManifest.cs
+++ src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest/PublishVerticalManifest.cs
@@ -15,13 +15,13 @@ namespace Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest
     public class PublishVerticalManifest : Task
     {
         [Required]
-        public ITaskItem[] AssetManifestPath { get; set; }
+        public ITaskItem[] AssetManifest { get; set; }
         [Required]
         public string VerticalAssetManifestOutputPath { get; set; }
 
         public override bool Execute()
         {
-            var assetManifestXmls = AssetManifestPath.Select(xmlPath => XDocument.Load(xmlPath.ItemSpec)).ToList();
+            var assetManifestXmls = AssetManifest.Select(xmlPath => XDocument.Load(xmlPath.ItemSpec)).ToList();
 
             VerifyAssetManifests(assetManifestXmls);
 
@@ -60,9 +60,16 @@ namespace Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest
                 .Select(attribute => attribute.ToString())
                 .ToHashSet();
 
+            if (assetManifestXmls.Skip(1).Any(manifest => manifest.Root.Attributes.Count() != rootAttributes.Count))
+            {
+                throw new ArgumentException("The asset manifests do not have the same number of root attributes.");
+            }
+
             if (assetManifestXmls.Skip(1).Any(assetManifestXml => 
-                    !assetManifestXml.Root.Attributes().All(attribute => 
-                        rootAttributes.Contains(attribute.ToString()))))
+                    !assetManifestXml.Root.Attributes().Select(attribute => attribute.ToString()).All(attribute =>
+                        // Ignore BuildId and AzureDevOpsBuildNumber attributes, they're different for different repos, 
+                        // TODO this should be fixed with https://github.com/dotnet/source-build/issues/3934
+                        rootAttributes.Contains(attribute) || attribute.Contains("BuildId") || attribute.Contains("AzureDevOpsBuildNumber"))))
             {
                 throw new ArgumentException("The asset manifests do not have the same root attributes.");
             }
