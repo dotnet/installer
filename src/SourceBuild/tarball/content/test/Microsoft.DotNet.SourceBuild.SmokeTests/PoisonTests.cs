eb4b6a0926d78090a3701f1fa10c2e246bddbed4
ashwinik30 <Ashwini.Kadam@ibm.com>
2024-03-20 14:31:43 +0530

    Update PoisonTests.cs

--- src/SourceBuild/tarball/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/PoisonTests.cs
+++ src/SourceBuild/tarball/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/PoisonTests.cs
@@ -14,7 +14,7 @@ namespace Microsoft.DotNet.SourceBuild.SmokeTests
         public PoisonTests(ITestOutputHelper outputHelper) : base(outputHelper) { }
 
         [SkippableFact(Config.PoisonReportPathEnv, skipOnNullOrWhiteSpaceEnv: true)]
-	public void VerifyUsage()
+	    public void VerifyUsage()
         {
             if (!File.Exists(Config.PoisonReportPath))
             {
