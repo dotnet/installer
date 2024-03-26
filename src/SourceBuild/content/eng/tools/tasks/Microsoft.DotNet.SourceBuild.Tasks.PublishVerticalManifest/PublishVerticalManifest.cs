d8888b21c1d968f90b8c3b1bf329562bd3bc788a
dkurepa <dkurepa@microsoft.com>
2024-03-26 12:56:00 +0100

    Address comments

--- src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest/PublishVerticalManifest.cs
+++ src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest/PublishVerticalManifest.cs
@@ -14,31 +14,43 @@ namespace Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest
 {
     public class PublishVerticalManifest : Task
     {
+        /// <summary>
+        /// AssetManifest paths
+        /// </summary>
         [Required]
-        public ITaskItem[] AssetManifest { get; set; }
+        public required ITaskItem[] AssetManifest { get; init; }
+
+        /// <summary>
+        /// Merged asset manifest output path
+        /// </summary>
         [Required]
-        public string VerticalAssetManifestOutputPath { get; set; }
+        public required string MergedAssetManifestOutputPath { get; init; }
+
+        /// <summary>
+        /// Azure DevOps build number
+        /// </summary>
         [Required]
-        public string VmrBuildNumber { get; set; }
+        public required string VmrBuildNumber { get; init; }
 
         private static readonly string _buildIdAttribute = "BuildId";
         private static readonly string _azureDevOpsBuildNumberAttribute = "AzureDevOpsBuildNumber";
-        private static readonly string[] _ignoredAttributes = new string[] { _buildIdAttribute, _azureDevOpsBuildNumberAttribute, "IsReleaseOnlyPackageVersion" };
+        private static readonly string[] _ignoredAttributes = [_buildIdAttribute, _azureDevOpsBuildNumberAttribute, "IsReleaseOnlyPackageVersion"];
 
         public override bool Execute()
         {
-            var assetManifestXmls = AssetManifest.Select(xmlPath => XDocument.Load(xmlPath.ItemSpec)).ToList();
+            List<XDocument> assetManifestXmls = AssetManifest.Select(xmlPath => XDocument.Load(xmlPath.ItemSpec)).ToList();
 
             VerifyAssetManifests(assetManifestXmls);
 
-            var verticalManifestRoot = assetManifestXmls.First().Root;
+            XElement mergedManifestRoot = assetManifestXmls.First().Root 
+                ?? throw new ArgumentException("The root element of the asset manifest is null.");
 
             // Set the BuildId and AzureDevOpsBuildNumber attributes to the value of VmrBuildNumber
-            verticalManifestRoot.SetAttributeValue(_buildIdAttribute, VmrBuildNumber);
-            verticalManifestRoot.SetAttributeValue(_azureDevOpsBuildNumberAttribute, VmrBuildNumber);
+            mergedManifestRoot.SetAttributeValue(_buildIdAttribute, VmrBuildNumber);
+            mergedManifestRoot.SetAttributeValue(_azureDevOpsBuildNumberAttribute, VmrBuildNumber);
 
-            var packageElements = new List<XElement>();
-            var blobElements = new List<XElement>();
+            List<XElement> packageElements = new();
+            List<XElement> blobElements = new();
 
             foreach (var assetManifestXml in assetManifestXmls)
             {
@@ -46,40 +58,43 @@ namespace Microsoft.DotNet.SourceBuild.Tasks.PublishVerticalManifest
                 blobElements.AddRange(assetManifestXml.Descendants("Blob"));
             }
             
-            packageElements = packageElements.OrderBy(packageElement => packageElement.Attribute("Id").Value).ToList();
-            blobElements = blobElements.OrderBy(blobElement => blobElement.Attribute("Id").Value).ToList();
+            packageElements = packageElements.OrderBy(packageElement => packageElement.Attribute("Id")?.Value).ToList();
+            blobElements = blobElements.OrderBy(blobElement => blobElement.Attribute("Id")?.Value).ToList();
 
-            var verticalManifest = new XDocument(new XElement(verticalManifestRoot.Name, verticalManifestRoot.Attributes(), packageElements, blobElements));
+            XDocument verticalManifest = new(new XElement(mergedManifestRoot.Name, mergedManifestRoot.Attributes(), packageElements, blobElements));
 
-            File.WriteAllText(VerticalAssetManifestOutputPath, verticalManifest.ToString());
+            File.WriteAllText(MergedAssetManifestOutputPath, verticalManifest.ToString());
 
             return true;
         }
 
-        private static void VerifyAssetManifests(List<XDocument> assetManifestXmls)
+        private static void VerifyAssetManifests(IReadOnlyList<XDocument> assetManifestXmls)
         {
             if (assetManifestXmls.Count == 0)
             {
                 throw new ArgumentException("No asset manifests were provided.");
             }
 
-            var rootAttributes = assetManifestXmls
+            HashSet<string> rootAttributes = assetManifestXmls
                 .First()
-                .Root
+                .Root?
                 .Attributes()
                 .Select(attribute => attribute.ToString())
-                .ToHashSet();
+                .ToHashSet() 
+                ?? throw new ArgumentException("The root element of the asset manifest is null.");
 
-            if (assetManifestXmls.Skip(1).Any(manifest => manifest.Root.Attributes().Count() != rootAttributes.Count))
+            if (assetManifestXmls.Skip(1).Any(manifest => manifest.Root?.Attributes().Count() != rootAttributes.Count))
             {
                 throw new ArgumentException("The asset manifests do not have the same number of root attributes.");
             }
 
             if (assetManifestXmls.Skip(1).Any(assetManifestXml => 
-                    !assetManifestXml.Root.Attributes().Select(attribute => attribute.ToString()).All(attribute =>
-                        // Ignore BuildId and AzureDevOpsBuildNumber attributes, they're different for different repos, 
-                        // TODO this should be fixed with https://github.com/dotnet/source-build/issues/3934
-                        _ignoredAttributes.Any(ignoredAttribute => attribute.StartsWith(ignoredAttribute)) || rootAttributes.Contains(attribute))))
+                    !assetManifestXml.Root?.Attributes().Select(attribute => attribute.ToString())
+                        .All(attribute =>
+                            // Ignore BuildId and AzureDevOpsBuildNumber attributes, they're different for different repos, 
+                            // TODO this should be fixed with https://github.com/dotnet/source-build/issues/3934
+                            _ignoredAttributes.Any(ignoredAttribute => attribute.StartsWith(ignoredAttribute)) || rootAttributes.Contains(attribute))
+                        ?? false))
             {
                 throw new ArgumentException("The asset manifests do not have the same root attributes.");
             }
