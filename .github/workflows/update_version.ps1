3f21cce91c6b7a636e1868e4bc7819fa4cea54a4
Jason Zhai <v-wuzhai@microsoft.com>
2024-06-17 23:40:54 -0700

    Automatically update SourceBuildPrebuiltBaseline.xml

---
+++ .github/workflows/update_version.ps1
@@ -0,0 +1,18 @@
+# Read the Versions.props file
+[xml]$versionsProps = Get-Content -Path 'Versions.props'
+
+# Get the value of MicrosoftBuildPackageVersion
+$version = $versionsProps.Project.PropertyGroup.MicrosoftBuildPackageVersion
+
+# Read the SourceBuildPrebuiltBaseline.xml file
+[xml]$sourceBuild = Get-Content -Path 'SourceBuildPrebuiltBaseline.xml'
+
+# Replace all '17.11.0-preview-24313-03' in IdentityGlob
+foreach ($usagePattern in $sourceBuild.DocumentElement.UsagePattern) {
+    if ($usagePattern.IdentityGlob -like '*17.11.0-preview-24313-03*') {
+        $usagePattern.IdentityGlob = $usagePattern.IdentityGlob.Replace('17.11.0-preview-24313-03', $version)
+    }
+}
+
+# Save the modified SourceBuildPrebuiltBaseline.xml file
+$sourceBuild.Save('SourceBuildPrebuiltBaseline.xml')
